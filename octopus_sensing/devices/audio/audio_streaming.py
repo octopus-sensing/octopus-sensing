329219f8c6e3cbbfb4312d0568d3052cfb9ab8af
Aidin Gharibnavaz <aidin@aidinhut.com>
2020-08-18 18:51:05 +1200

    Add GPLv3 license

--- octopus_sensing/devices/audio/audio_streaming.py
+++ octopus_sensing/devices/audio/audio_streaming.py
@@ -1,3 +1,17 @@
+# This file is part of Octopus Sensing <https://octopus-sensing.nastaran-saffar.me/>
+# Copyright © Zahra Saffaryazdi 2020
+#
+# Octopus Sensing is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+#  either version 3 of the License, or (at your option) any later version.
+#
+# Octopus Sensing is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Foobar.
+# If not, see <https://www.gnu.org/licenses/>.
+
 import sounddevice as sd
 from scipy.io.wavfile import write
 from threading import Thread
