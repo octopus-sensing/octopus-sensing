bac435aaa60aa062fd83e44a7a4370b8212ba309
Nastaran <nsaffar@gmail.com>
2020-12-08 16:15:34 +1300

    Changing Foobar to Octopus Sensing

--- octopus_sensing/questionnaire/text_question.py
+++ octopus_sensing/questionnaire/text_question.py
@@ -9,7 +9,7 @@
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along with Foobar.
+# You should have received a copy of the GNU General Public License along with Octopus Sensing.
 # If not, see <https://www.gnu.org/licenses/>.
 import gi
 gi.require_version('Gtk', '3.0')  # nopep8
