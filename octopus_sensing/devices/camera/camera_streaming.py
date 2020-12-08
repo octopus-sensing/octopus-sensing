bdfdb655ac1a14c3bf506b2049d02eb6b963f10d
Nastaran <nsaffar@gmail.com>
2020-12-08 16:00:37 +1300

    Adding text question and fixing some bugs

--- octopus_sensing/devices/camera/camera_streaming.py
+++ octopus_sensing/devices/camera/camera_streaming.py
@@ -18,6 +18,9 @@ import threading
 import cv2
 from octopus_sensing.devices.device import Device
 from octopus_sensing.common.message_creators import MessageType
+import time
+import csv
+import datetime
 
 
 class CameraStreaming(Device):
@@ -25,21 +28,23 @@ class CameraStreaming(Device):
         assert (camera_no is not None) ^ (camera_path is not None), \
             "Only one of camera_no or camera_path should have value"
         super().__init__(**kwargs)
-
         self.output_path = os.path.join(self.output_path, "video")
         os.makedirs(self.output_path, exist_ok=True)
         if camera_no is not None:
             self._camera_number = camera_no
-        else:
-            self._camera_number = os.path.realpath(camera_path)
+        elif camera_path is not None:
+            self._camera_number = int(os.path.realpath(camera_path))
 
         self._image_width = image_width
         self._image_height = image_height
         self._video_size: Tuple[int, int] = (self._image_width, self._image_height)
         self._video_capture: Any = None
         self._fps: int = 30
+        self._capture_times: list = []
+        self._counter = 0
 
     def _run(self):
+        print("self._camera_number", self._camera_number)
         self._video_capture = cv2.VideoCapture(self._camera_number)
         try:
             self._video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, self._image_width)
@@ -54,17 +59,19 @@ class CameraStreaming(Device):
         self._fps = self._video_capture.get(cv2.CAP_PROP_FPS)
         self._video_size = (int(self._video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
                             int(self._video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        self._video_capture.read()
 
         recording_thread = None
         recording_event = None
 
-        #print(f"Initialized video device [{self.name}]: {self._video_size} fps: {self._fps}")
+        print(f"Initialized video device [{self.name}]: {self._video_size} fps: {self._fps}")
 
         while True:
             message = self.message_queue.get()
             if message is None:
                 continue
             if message.type == MessageType.START:
+                self._capture_times = []
                 if recording_thread is not None:
                     raise RuntimeError(
                         ("[{0} device] Received two start messages. "
@@ -79,9 +86,11 @@ class CameraStreaming(Device):
                 recording_event.set()
                 recording_thread = threading.Thread(
                     target=self._stream_loop, args=(file_name, recording_event), daemon=True)
+                print("after thread", time.time())
                 recording_thread.start()
 
             elif message.type == MessageType.STOP:
+                self._save_frame_times(file_name)
                 if recording_event is not None:
                     recording_event.clear()
                 recording_thread = None
@@ -104,12 +113,14 @@ class CameraStreaming(Device):
                                  coded,
                                  self._fps,
                                  self._video_size)
-        self._video_capture.read()
+
         try:
             while self._video_capture.isOpened:
                 if event.is_set():
                     ret, frame = self._video_capture.read()
                     if ret:
+                        self._counter += 1
+                        self._capture_times.append(datetime.datetime.now())
                         writer.write(frame)
                 else:
                     writer.release()
@@ -118,3 +129,30 @@ class CameraStreaming(Device):
         except Exception as error:
             print("Error while recording video. Device: {0}".format(self.name))
             print(error)
+
+    def _stream_loop_image(self, file_name: str, event: threading.Event):
+        try:
+            while self._video_capture.isOpened:
+                if event.is_set():
+                    ret, frame = self._video_capture.read()
+                    if ret:
+                        a = time.time()
+                        os.makedirs(file_name[:-4], exist_ok=True)
+                        image_file_name = file_name[:-4] + "/" + str(a) + ".jpg"
+                        print(image_file_name)
+                        cv2.imwrite(image_file_name, frame)
+                else:
+                    break
+
+        except Exception as error:
+            print("Error while recording video. Device: {0}".format(self.name))
+            print(error)
+
+    def _save_frame_times(self, file_name):
+        print(len(self._capture_times))
+        csv_file_name = file_name[:-3] + "csv"
+        with open(csv_file_name, 'a') as csv_file:
+            for item in self._capture_times:
+                writer = csv.writer(csv_file)
+                writer.writerow([item])
+                csv_file.flush()
