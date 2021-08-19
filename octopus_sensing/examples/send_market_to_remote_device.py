b37d8aeb3220a3e6510184e3c6f77f0d89f9e607
Nastaran <nsaffar@gmail.com>
2021-08-19 14:52:40 +1200

    Remove Pipfile, add example

---
+++ octopus_sensing/examples/send_market_to_remote_device.py
@@ -0,0 +1,48 @@
+# This file is part of Octopus Sensing <https://octopus-sensing.nastaran-saffar.me/>
+# Copyright © Nastaran Saffaryazdi 2020
+#
+# Octopus Sensing is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+#  either version 3 of the License, or (at your option) any later version.
+#
+# Octopus Sensing is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Foobar.
+# If not, see <https://www.gnu.org/licenses/>.
+
+import time
+from octopus_sensing.device_coordinator import DeviceCoordinator
+from octopus_sensing.devices.network_devices.socket_device import SocketNetworkDevice
+from octopus_sensing.common.message_creators import start_message, stop_message
+
+def server():
+    '''
+    Starts a server and sends several messages and terminates
+    '''
+    device_coordinator = DeviceCoordinator()
+    socket_device = SocketNetworkDevice("localhost", 5002)
+    device_coordinator.add_devices([socket_device])
+
+    input("Press enter to start sending markers")
+
+    time.sleep(5)
+    message = start_message("test", "00")
+    device_coordinator.dispatch(message)
+    
+    time.sleep(2)
+    message = stop_message("test", "00")
+    device_coordinator.dispatch(message)
+    time.sleep(2)
+    message = start_message("test", "01")
+    device_coordinator.dispatch(message)
+    time.sleep(2)
+    message = stop_message("test", "01")
+    device_coordinator.dispatch(message)
+    time.sleep(3)
+
+    device_coordinator.terminate()
+
+if __name__ == "__main__":
+    server()
