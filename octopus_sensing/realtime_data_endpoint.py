2b4420b4105525a0355a9eb779b6a10c7ece003c
Nastaran <nsaffar@gmail.com>
2022-06-10 14:06:34 +1200

    Fixing a mypy error

--- octopus_sensing/realtime_data_endpoint.py
+++ octopus_sensing/realtime_data_endpoint.py
@@ -24,11 +24,11 @@ class RealtimeDataEndpoint(EndpointBase):
                          port=9330, get_callback=self._get_handler)
         self._device_coordinator = device_coordinator
 
-    def _get_handler(self, request_reader, query_params: Dict[str, str]):
-        duration: int = int(query_params.get('duration', 3)[0])
+    def _get_handler(self, request_reader, query_params: Dict[str, List[str]]):
+        duration: int = int(query_params.get('duration', ["3"])[0])
 
-        device_list_string = query_params.get('device_list', None)[0]
+        device_list_string = query_params.get('device_list', [""])[0]
         device_list: Optional[List[str]] = None
-        if device_list_string is not None:
+        if device_list_string != "":
             device_list = device_list_string.split(',')
         return self._device_coordinator.get_realtime_data(duration, device_list)
