b7f199282efdd063cd397f5547c7934a03a5fee5
Nastaran <nsaffar@gmail.com>
2026-02-11 07:53:29 +1300

    Changing the version because of changing the installation

--- octopus_sensing/__init__.py
+++ octopus_sensing/__init__.py
@@ -12,4 +12,4 @@
 # You should have received a copy of the GNU General Public License along with Octopus Sensing.
 # If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '4.4.5'
+__version__ = '5.0.0'
